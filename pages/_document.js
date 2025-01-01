8530790542ea7fa63b026e22d0644ec46799a809
AlignConnect <devlopersalignconnect@gmail.com>
2025-01-01 11:06:49 +0530

    latest code

--- pages/_document.js
+++ pages/_document.js
@@ -126,6 +126,26 @@ export default function Document() {
     })(window, document, "clarity", "script", "p825zsng4y");
                 `}
         </Script>
+
+        <Script strategy="lazyOnload">
+          {`
+         !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '923630475929962');
+          fbq('track', 'PageView');
+        `}
+        </Script>
+
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.facebook.com/tr?id=923630475929962&ev=PageView&noscript=1`}
+        />
       </Head>
       <body>
         <Main />
